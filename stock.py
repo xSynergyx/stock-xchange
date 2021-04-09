f76125ed03c63334ed76f04644e21a889a222873
Chris Corprew <ec2-user@ip-172-31-33-103.ec2.internal>
2021-04-09 03:50:31 +0000

    Modified stock.py by adding loop to handle batch requests to stock api and added NYT url

--- stock.py
+++ stock.py
@@ -3,8 +3,8 @@
 
     Stock class file.
 """
-import requests
 from random import randint
+import requests
 import pandas as pd #Add to requirements pandas and numpy
 import time
 import os
@@ -13,7 +13,9 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 class Stock:
-    BASE_URL = "https://sandbox.iexapis.com/stable/stock/market/batch?"
+    """Class object gathers stock information and news for selected companies"""
+    IEX_SANDBOX_URL = "https://sandbox.iexapis.com/stable/stock/market/batch?"
+    NYT_URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
     
     def symbols(self, csv_file):
         """Gathers symbols into list from csv file"""
@@ -36,28 +38,37 @@ class Stock:
         energy_stock = self.symbols('energy.csv')
         finance_stock = self.symbols('finance.csv')
         utilities_stock = self.symbols('pub_utilities.csv')
-        print('mega: ' + str(mega_stock))
-        print('tech: ' + str(tech_stock))
-        print('energy: ' + str(energy_stock))
-        print('finance: ' + str(finance_stock))
-        print('public utilities: ' + str(utilities_stock))
+        # print('mega: ' + str(mega_stock))
+        # print('tech: ' + str(tech_stock))
+        # print('energy: ' + str(energy_stock))
+        # print('finance: ' + str(finance_stock))
+        # print('public utilities: ' + str(utilities_stock))
+        home_lst.append({'Mega': self.search(mega_stock)})
         return home_lst
 
     def search(self, query):
         """Takes list of symbols, and gathers stock information"""
         data = {}
         symbols = None
+        if len(query) == 1:
+            symbols = query[0]
+        else:
+            symbols = query[0]
+            for i in range(1, len(query)):
+                symbols += ',{}'.format(query[i])
         params = {
          'symbols': symbols,
          'types': 'quote',
          'token': os.getenv('IEX_CLOUD_SANDBOX_KEY')
         }
-        pass
-   
+        stocks = [x.upper() for x in query] #capatalize symbols for json file
+        response = requests.get(self.IEX_SANDBOX_URL, params=params)
+        response_json = response.json()
+        print(response_json)
+        
     def news(self, query):
         """ Gathers articles related to company after click """
         pass
 
-    
-test = Stock()
-print(test.default())
+TEST = Stock()
+print(TEST.default())
