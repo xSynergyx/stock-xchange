ff28fc3eb4b7a771d7c9dc5546cee7bfaec4cd99
Chris Corprew <ec2-user@ip-172-31-33-103.ec2.internal>
2021-04-10 01:55:14 +0000

    Added .pylintrc file, and modified stock.py by making pass lint tests and moved symbol function outside of the stock class

--- stock.py
+++ stock.py
@@ -4,40 +4,40 @@
     Stock class file.
 """
 from random import randint
+#import time
+import os
 import requests
 import pandas as pd #Add to requirements pandas and numpy
-import time
-import os
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
+
+def symbols(csv_file):
+    """Gathers symbols into list from csv file"""
+    df = pd.read_csv(csv_file)
+    symbol = df['Symbol']
+    stock_lst = []
+    num_list = []
+    while len(stock_lst) < 4:
+        num = randint(0, len(symbol)-1)
+        if num not in num_list:
+            stock_lst.append(symbol[num])
+            num_list.append(num)
+    return stock_lst
 
 class Stock:
     """Class object gathers stock information and news for selected companies"""
     IEX_SANDBOX_URL = "https://sandbox.iexapis.com/stable/stock/market/batch?"
     NYT_URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
-    
-    def symbols(self, csv_file):
-        """Gathers symbols into list from csv file"""
-        df = pd.read_csv(csv_file)
-        symbols = df['Symbol']
-        stock_lst = []
-        num_list = []
-        while len(stock_lst) < 4:
-            num = randint(0, len(symbols)-1)
-            if num not in num_list:
-                stock_lst.append(symbols[num])
-                num_list.append(num)
-        return stock_lst
-   
+
     def default(self):
         """Default homescreen with stock information"""
         home_lst = []
-        mega_stock = self.symbols('stock_categories/mega.csv')
-        tech_stock = self.symbols('stock_categories/tech.csv')
-        energy_stock = self.symbols('stock_categories/energy.csv')
-        finance_stock = self.symbols('stock_categories/finance.csv')
-        utilities_stock = self.symbols('stock_categories/pub_utilities.csv')
+        mega_stock = symbols('stock_categories/mega.csv')
+        tech_stock = symbols('stock_categories/tech.csv')
+        energy_stock = symbols('stock_categories/energy.csv')
+        finance_stock = symbols('stock_categories/finance.csv')
+        utilities_stock = symbols('stock_categories/pub_utilities.csv')
         home_lst.append({'Mega': self.search(mega_stock)})
         home_lst.append({'Tech': self.search(tech_stock)})
         home_lst.append({'Energy': self.search(energy_stock)})
@@ -48,17 +48,17 @@ class Stock:
     def search(self, query):
         """Takes list of symbols, and gathers stock information"""
         data = {}
-        symbols = None
+        stock_symbols = None
         if len(query) == 1:
-            symbols = query[0]
+            stock_symbols = query[0]
         else:
-            symbols = query[0]
+            stock_symbols = query[0]
             for i in range(1, len(query)):
-                symbols += ',{}'.format(query[i])
+                stock_symbols += ',{}'.format(query[i])
         params = {
-         'symbols': symbols,
-         'types': 'quote',
-         'token': os.getenv('IEX_CLOUD_SANDBOX_KEY')
+            'symbols': stock_symbols,
+            'types': 'quote',
+            'token': os.getenv('IEX_CLOUD_SANDBOX_KEY')
         }
         stocks = [x.upper() for x in query] #capatalize symbols for json file
         response = requests.get(self.IEX_SANDBOX_URL, params=params)
@@ -67,7 +67,6 @@ class Stock:
         for stock in stocks:
             stock_dict = {}
             stock_quote = response_json[stock]['quote']
-            #print(stock_quote)
             stock_dict['Symbol'] = stock
             stock_dict['Company'] = stock_quote['companyName']
             stock_dict['High'] = stock_quote['high']
@@ -75,12 +74,18 @@ class Stock:
             stock_dict['Price'] = stock_quote['latestPrice']
             data[stock] = stock_dict
         return data
-        
-    def news(self, query):
+
+    def news_nyt(self, stock):
         """ Gathers articles related to company after click """
-        pass
+        #news = {}
+        #return news
+
+    def news_iex(self, stock):
+        """ Back up news method with IEX API incase NYT doesn't work """
+        #news = {}
+        #return news
 
 TEST = Stock()
-for i in TEST.default():
-    print(str(i) + '\n')
+for j in TEST.default():
+    print(str(j) + '\n')
 #print(TEST.default())
