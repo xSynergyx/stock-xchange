7fd278370d8d906c794757c53c327b782a12e014
Christian Medina <ec2-user@ip-172-31-37-118.ec2.internal>
2021-04-16 22:20:34 +0000

    Added jest test to check for the main two components of the landing page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Stock XChange logo and Login button in displayed', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  const joinElement = screen.getByText('Stock XChange');
+  expect(joinElement).toBeInTheDocument();
+  
+  const joinButtonElement = screen.getByText('Login');
+  expect(joinButtonElement).toBeInTheDocument();
+  
+  //fireEvent.click(joinButtonElement);
 });
