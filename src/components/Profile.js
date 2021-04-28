3fc94e9010abc2a9a136abe491efa94bd2d84321
Christian Medina <ec2-user@ip-172-31-37-118.ec2.internal>
2021-04-28 17:22:41 +0000

    Created a logo and added it to the navbar. Set the outline for the profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,25 @@
 import './Profile.css';
-import React from 'react';
+import React, { useState } from 'react';
+import { MdEdit } from 'react-icons/md';
 
-function Profile() {
-    return (<div>Profile page</div>);
+function Profile(props) { // Have to get the username and bio from the DB
+    return (
+        <div className="grid-container">
+            <div className="personal">
+                <h1>Profile</h1>
+                <div><p>Name: { props.username } <MdEdit /></p> </div>
+                <p>Username: { props.email } <MdEdit /></p> 
+                <p>Bio: <MdEdit /></p>
+            </div>
+            <div className="favorites">
+                <h1>Some Profile Thing(?)</h1>
+                <p>Thought it was gonna be stocks here. Going to have to change it</p>
+            </div>
+            <div className="investor-preferences">
+                <h1>Investing Style</h1>
+                <p>Preferred stock information</p>
+            </div>
+        </div>);
 }
 
 export default Profile;
